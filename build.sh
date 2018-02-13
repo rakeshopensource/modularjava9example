d690f90eb48b0c5dfbbcc294a2014b0ce33daa49
rakeshrathi <rakesh.rathi@snapdeal.com>
2018-02-13 12:30:20 +0530

    java home path corrected

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+export JAVA_HOME=$(/usr/libexec/java_home -v 9)
+export PATH="$JAVA_HOME/bin:$PATH"
+
 /bin/rm -rf output
 
 mkdir -p output/mlib
@@ -18,8 +21,6 @@ jar -c -f output/mlib/helloclient.jar --main-class com.hello.client.HelloClient 
 #java -p output/mlib -m com.hello.client.HelloClient
 java -p output/mlib -m com.hello.client/com.hello.client.HelloClient
 
-export JAVA_HOME=$(/usr/libexec/java_home -v 9)
-export PATH="$JAVA_HOME/bin:$PATH"
 
 /bin/rm -rf helloclientimage/
 jlink --module-path /Library/Java/JavaVirtualMachines/jdk-9.0.4.jdk/Contents/Home/jmods:output/mlib --add-modules com.hello --add-modules com.hello.client --output helloclientimage --launcher=helloclient=com.hello.client
